d51fff8bacd59573da1c5c858d6fc1e3127b904c
SLphil <pjackson@softlayer.com>
2013-03-27 09:54:08 -0500

    Even more stuffs

--- stubby.py
+++ stubby.py
@@ -24,6 +24,10 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
+login_manager = LoginManager()
+login_manager.setup_app(app)
+
+login_manager.login_view = "login"
 
 
 def connect_db():
@@ -44,7 +48,9 @@ def before_request():
 def teardown_request(exception):
     g.db.close()
 
+
 @app.route('/admin')
+@login_required
 def show_entries():
     cur = g.db.execute('select uri, short_url from entries order by id desc')
     entries = [dict(uri=row[0], short_url=row[1]) for row in cur.fetchall()]
@@ -57,17 +63,14 @@ def redirect_short_url(short_url):
     return redirect(url)
 
 @app.route('/add', methods=['POST'])
+@login_required
 def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
     g.db.execute('insert into entries (uri, short_url) values (?, ?)',
                  [request.form['uri'], request.form['short_url']])
     g.db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
-login_manager = LoginManager()
-login_manager.setup_app(app)
 
 @login_manager.user_loader
 def load_user(userid):
@@ -79,9 +82,12 @@ def login():
     if request.method == 'POST':
         # login and validate the user...
         user = User(form.username.data, form.password.data)
-        login_user(user) # the user object from DB
-        flash("Logged in successfully.")
-        return redirect(request.args.get("next") or url_for("admin"))
+        if login_user(user):
+            flash("Logged in successfully.")
+        else:
+            flash("fucked")
+            return redirect(url_for("login"))
+        return redirect(request.args.get("next") or url_for("show_entries"))
     return render_template("login.html", form=form)
 
 
@@ -89,26 +95,35 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for("index"))
+    return redirect(url_for("show_entries"))
 
-class LoginForm():
+class LoginForm(Form):
     username = TextField('Username')
-    password = TextField('Password')
+    password = PasswordField('Password')
 
 
 class User(UserMixin):
-    def __init__(seflf, username, password, email):
+    def __init__(self, username, password, email=None):
         self.username = username
         self.password = password
         self.email = email
+        self.id = 1
+        self.is_valid = False
 
-    def login(self, username, password):
-        ## sql look up compare with vars
+    def is_authenticated(self):
+        return self.is_valid
+
+    def is_active(self):
+        ## sql look up compare with vars and set ID
         return True
 
-    def get(self, userid):
-        ## sql look up get user object, id, username, email
-        return True
+    @classmethod
+    def get(self, user_id):
+        ## db grab that user
+        new_user = self("phil", "", "test@gmail.com")
+        new_user.is_valid = True
+        return new_user
+
 
 if __name__ == '__main__':
     app.run()
