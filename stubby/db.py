d44d72c665dd05ab8791904f4a88035d373016ce
SLphil <pjackson@softlayer.com>
2013-04-15 21:23:41 -0500

    few more

---
+++ stubby/db.py
@@ -0,0 +1,14 @@
+from flask import g, app
+from contextlib import closing
+import sqlite3
+
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
---
+++ stubby/stub.py
@@ -0,0 +1,38 @@
+class Stub():
+    def __init__(self, url_source=None, url_stub=None):
+        self.url_source = url_source
+        self.url_stub = url_stub
+
+    def add(self):
+        g.db.execute('insert into stubs (url_source, url_stub) values (?, ?)',
+            [self.url_source, self.url_stub])
+        try:
+            g.db.commit()
+            flash("Stub %s Added" % self.url_stub)
+            return True
+        except:
+            flash("Stub creation failed")
+            return False
+
+    @classmethod
+    def get(self, url_stub):
+        stub = False
+        cur = g.db.execute('select url_source, url_stub from stubs where url_stub=?',
+            [url_stub])
+        res = cur.fetchone()
+        if res:
+            stub = self(res[0], res[1])
+        return stub
+
+    def remove(self):
+        g.db.execute('delete from stubs where url_stub=? and url_source=?',
+            [self.url_stub, self.url_source])
+        try:
+            g.db.commit()
+            return True
+        except:
+            return False
+
+    def log(self):
+        # log request
+        return
