92b426c69d0b9add6e1d36c40a22beed8449bb5d
SLphil <pjackson@softlayer.com>
2013-04-24 21:22:43 -0500

    stuffs

--- stubby/__init__.py
+++ stubby/__init__.py
@@ -1,19 +1,9 @@
 # all the imports
 from __future__ import with_statement
 from flask import Flask, request, session, g, redirect, url_for, \
-     abort, render_template, flash, Blueprint
-from wtforms import Form, BooleanField, TextField, PasswordField, validators
-
-from contextlib import closing
-from flask.ext.login import (LoginManager, current_user, login_required,
-                            login_user, logout_user, UserMixin, AnonymousUser,
-                            confirm_login, fresh_login_required)
+     abort, flash, Blueprint
 from db import connect_db
 from pprint import pprint as pp
-
-DATABASE = '/tmp/redirect.db'
-DEBUG = True
-SECRET_KEY = 'development key'
 
 
 import admin
@@ -21,16 +11,22 @@ import stubdirect
 import db
 import stub
 import stubuser
+import config
 
-app = Flask(__name__)
-app.debug = True
-app.config.from_object(__name__)
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+app = Flask(__name__, instance_relative_config=True)
+
+
 app.register_blueprint(admin.blueprint, url_prefix='/admin')
 app.register_blueprint(stubdirect.blueprint)
 app.register_blueprint(db.blueprint)
 app.register_blueprint(stub.blueprint)
 app.register_blueprint(stubuser.blueprint)
+app.register_blueprint(config.blueprint)
+
+
+app.config.from_object("config")
+app.config.from_pyfyle("application.cfg", silent=True)
 
 
 @app.before_request
--- stubby/db.py
+++ stubby/db.py
@@ -1,4 +1,4 @@
-from flask import g, app, Blueprint, current_app
+from flask import g, Blueprint, current_app, app
 from contextlib import closing
 import sqlite3
 
@@ -6,11 +6,11 @@ blueprint = Blueprint('db', __name__)
 
 
 def connect_db():
-    return sqlite3.connect(current_app.config['DATABASE'])
+    return sqlite3.connect(app.config['DATABASE'])
 
 
 def init_db():
     with closing(connect_db()) as db:
-        with app.open_resource('schema.sql') as f:
+        with app.open_resource('../schema.sql') as f:
             db.cursor().executescript(f.read())
         db.commit()
